051ad0b3514f1de65d3f0d0bb2786f0e4fd400bb
Fernando Romero <fer.rom.mu@gmail.com>
2023-02-19 14:47:06 -0300

    Add yup validation schema to add user modal.

--- src/components/home/AddFriendModal.jsx
+++ src/components/home/AddFriendModal.jsx
@@ -1,6 +1,7 @@
 import { Button, Modal, ModalContent, ModalBody, ModalFooter, ModalHeader, ModalOverlay, ModalCloseButton } from "@chakra-ui/react";
 import { Form, Formik } from "formik";
 import TextField from '../TextField'
+import * as Yup from "yup";
 
 const AddFriendModal = ({ isOpen, onClose }) => {
   return (
@@ -9,17 +10,30 @@ const AddFriendModal = ({ isOpen, onClose }) => {
       <ModalContent>
         <ModalHeader>Add a friend</ModalHeader>
         <ModalCloseButton onClick={onClose} />
-        <Formik>
+        <Formik
+          initialValues={{ friendName: "" }}
+          validationSchema={Yup.object({
+            friendName: Yup.string()
+              .required('Username required')
+              .min(6, "Invalid username")
+              .max(28, "Invalid username")
+          })}
+          onSubmit={(values, actions) => {
+            alert(JSON.stringify(values, null, 2))
+            actions.resetForm()
+            onClose()
+          }}
+        >
           <Form>
             <ModalBody>
               <TextField
-                label="Fiend's name"
+                label="Friend's name"
                 placeholder="Enter friend's username"
                 autoComplete="off"
                 name="friendName"></TextField>
             </ModalBody>
             <ModalFooter>
-              <Button onClick={onClose} type="submit">
+              <Button type="submit">
                 Submit
               </Button>
             </ModalFooter>
